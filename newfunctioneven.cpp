1d9153edf8580b595dea98f376f227f2495a4535
Nidhi sahu27 <149890086+NidhiSahu2@users.noreply.github.com>
2024-02-17 22:20:46 +0530

    Add files via upload

---
+++ newfunctioneven.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int number( int num){
+	
+if(num%2==0){
+	
+cout<<" this a even number "<<endl;	
+	
+}
+else
+{
+cout<<"this a odd number"<<endl;
+		
+}
+return 0;
+}
+int main(){
+	
+number (8);
+	
+return 0;
+	
+
+}
