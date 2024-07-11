d0e39d32bd11e3b4202dea060ef478da150172a2
Nidhi Sahu27 <149890086+NidhiSahu2@users.noreply.github.com>
2024-07-12 00:00:08 +0530

    Add files via upload

---
+++ basichashinh2,cpp.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+class solutions
+{
+	unordered_map<int,int>map;
+	map["jhon"]=4;
+	map["johnny"]=4;
+	map["jamie"]=3;
+	map["jackle"]=2;
+	for(int i=0; i<n; i++)
+	{
+		int name = max_vote;
+		
+	}
+}
+
